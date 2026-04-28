867e35eb14ece810eacdb0d7e8a2f885af399973
juancho-018 <camilomeneses161@gmail.com>
2026-04-28 15:40:01 -0500

    feat: se agrego la migración de la tabla Tarea

--- database/migrations/2026_04_28_185157_tarea.php
+++ database/migrations/2026_04_28_185157_tarea.php
@@ -10,15 +10,24 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        //
-    }
+{
+    Schema::create('tareas', function (Blueprint $table) {
+        $table->id(); // bigIncrements PK
+        $table->string('titulo', 100); // string(100) obligatorio
+        $table->text('descripcion')->nullable(); // text opcional
+        $table->enum('prioridad', ['baja', 'media', 'alta'])->default('media'); // enum con default
+        $table->boolean('completada')->default(false); // boolean default false
+        $table->date('fecha_limite')->nullable(); // date opcional
+        $table->timestamps(); // created_at y updated_at
+    });
+}
+
 
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('tareas');
     }
 };
